14b215b12457ddd592cee4157099bb3fa8792682
Honey Sanghani <honeysanghani98@gmail.com>
2021-07-18 02:16:33 -0400

    added dynamic submit feature

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,11 +1,19 @@
-var buttonEl=document.querySelector("#save-task");
 var listEl=document.querySelector(".task-list");
 var formEl = document.querySelector("#task-form");
-function createTaskHandler(event){
+var createTaskHandler=function(event){
     event.preventDefault();
-    var taskListEl=document.createElement("li");
-    taskListEl.className = "task-item"; 
-    taskListEl.textContent = "This is a new task."; 
-    listEl.appendChild(taskListEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //creating list items
+    var listItemEl=document.createElement("li");
+    listItemEl.className="task-item";
+    //create div to hold content
+    var taskInfoEl=document.createElement("div");
+    taskInfoEl.className="task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    // add entire list item to list
+    listEl.appendChild(listItemEl);
+
 }
 formEl.addEventListener("submit", createTaskHandler);
